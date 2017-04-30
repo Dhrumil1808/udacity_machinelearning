894eb33006848c1dbc292a3a48ec3d75a78e5e1a
Dhrumil1808 <dhrml.shah@gmail.com>
2017-04-29 23:15:30 -0700

    Optimised SVM with Larger C

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,15 +19,13 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-features_train = features_train[:len(features_train)/100] 
-labels_train = labels_train[:len(labels_train)/100] 
-
-
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
 
 #########################################################
 ### your code goes here ###
 from sklearn.svm import SVC
-clf = SVC(kernel = "rbf")
+clf = SVC(kernel = "rbf",C=10000.0)
 clf.fit(features_train,labels_train)
 pred=clf.predict(features_test)
 
