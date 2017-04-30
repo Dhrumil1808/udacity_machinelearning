36743fcab2fcfaac54fccb5690f7a80318a78bb5
Dhrumil1808 <dhrml.shah@gmail.com>
2017-04-29 20:54:05 -0700

    Naive Bayes Machine Learning

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -11,9 +11,12 @@
 """
     
 import sys
+import random
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -22,10 +25,24 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+def splitDataset(dataset, splitRatio):
+	trainSize = int(len(dataset) * splitRatio)
+	trainSet = []
+	copy = list(dataset)
+	while len(trainSet) < trainSize:
+		index = random.randrange(len(copy))
+		trainSet.append(copy.pop(index))
+	return [trainSet, copy]
 
 
 #########################################################
 ### your code goes here ###
+clf=GaussianNB()
+clf.fit(features_train,labels_train)
+
+pre=clf.predict(features_test)
+print accuracy_score(labels_test,pre)
+
 
 
 #########################################################
